0d8c1780f72a791a337fb091dc66e753889a7293
oyamairishitomi <h.tezuka0707@gmail.com>
2026-05-29 19:55:06 +0900

    Add avatar upload feature using Active Storage

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,6 @@
 class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+  has_one_attached :avatar
+  validates :avatar, content_type: %i[jpg png gif], if: -> { avatar.attached? }
 end
